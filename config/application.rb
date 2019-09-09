f6141f6ba233b550cafca76736c920cce496b6aa
Alexandre Carneiro <alexandre.carneiro@xing.com>
2019-09-09 09:45:04 +0200

    Changed config file to snakecase incoming request

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+require_relative "../app/middlewares/snake_case_parameters"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +23,9 @@ module GobriefingBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.middleware.use SnakeCaseParameters
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
